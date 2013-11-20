b9b39486c72de9b9d34375756a05b58b0bcf7305
Bruno Wu <wubr2000@bruno-wus-macbook-pro.local>
2013-11-20 21:36:34 +0800

    testing data models

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GithubTest::Application.config.secret_key_base = 'b69e510e86703d79b6f8fd5633513f98245547de57e936e6d59b33031f56eca0a8cd366f5f832d22253f5429335d6ce39cfbd96632d41a3c86d34e29eeadeb08'
